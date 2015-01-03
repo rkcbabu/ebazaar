bc3b1e3e625a101acfed8c34fa327561e7285709
subhanshu <subhanshu@badaya>
2015-01-03 12:08:57 +0530

    small fix

--- src/main/java/mum/pm/ebazaar/repositoryImp/UserDaoImp.java
+++ src/main/java/mum/pm/ebazaar/repositoryImp/UserDaoImp.java
@@ -5,6 +5,6 @@ import mum.pm.ebazaar.domain.User;
 import mum.pm.ebazaar.repository.CategoryDao;
 import mum.pm.ebazaar.repository.UserDao;
 
-public class UserDaoImp extends GenericHibernateDaoImpl<User, Long> implements UserDao {
-
-}
+//public class UserDaoImp extends GenericHibernateDaoImpl<User, Long> implements UserDao {
+//
+//}
